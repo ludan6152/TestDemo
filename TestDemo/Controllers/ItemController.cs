f94b98733c44338443723be0aab9763b9ac0f8a7
ludan6152 <ludan6152@gmail.com>
2022-03-09 14:59:45 +0800

    顯示清單

--- TestDemo/Controllers/ItemController.cs
+++ TestDemo/Controllers/ItemController.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestDemo.Models;
 
 namespace TestDemo.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly IItemModel _itemModel;
+
+        public ItemController(IItemModel itemModel)
+        {
+            this._itemModel = itemModel;
+        }
+
         #region 畫面
 
         #region 主頁
         public ActionResult Item_Index()
         {
             return View();
+        }
+        #endregion
+
+        #endregion
+
+        #region 資料
+
+        #region 取得項目清單
+        public JsonResult Get_Item_List()
+        {
+            var List = _itemModel.Get_Item_List();
+
+            return Json(new { List }, JsonRequestBehavior.AllowGet);
         }
         #endregion
 
